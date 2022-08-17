834a70224bf328bf87cc55bb35a98dce69446f6e
dicetheDev <samuelblessing125@gmail.com>
2022-08-17 19:05:48 +0100

    new push

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -5,20 +5,21 @@ import "./IERC20.sol";
 
 contract SwapTokenTime {
 
-    IERC20 public token1;
+    IERC20 public blessingToken;
     address public owner1;
-    IERC20 public token2;
+    IERC20 public web3bridgeToken;
     address public owner2;
     uint public amount1;
     uint public amount2;
     uint public swapRate;
 
-    constructor(address _token1, address _owner1, uint256 _amount1, address _token2, address _owner2,
+    constructor(address _blessingToken, address _owner1, uint256 _amount1,
+         address _web3bridgeToken, address _owner2,
         uint256 _amount2) {
-        token1 = IERC20(_token1);
+        blessingToken = IERC20(_blessingToken);
         owner1 = _owner1;
         amount1 = _amount1;
-        token2 = IERC20(_token2);
+        web3bridgeToken = IERC20(_web3bridgeToken);
         owner2 = _owner2;
         amount2 = _amount2;
     }
@@ -26,16 +27,16 @@ contract SwapTokenTime {
     function swap() public {
         require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
         require(
-            token1.allowance(owner1, address(this)) >= amount1,
+            blessingToken.allowance(owner1, address(this)) >= amount1,
             "Token 1 allowance too low"
         );
         require(
-            token2.allowance(owner2, address(this)) >= amount2,
+            web3bridgeToken .allowance(owner2, address(this)) >= amount2,
             "Token 2 allowance too low"
         );
 
-        _safeTransferFrom(token1, owner1, owner2, amount1);
-        _safeTransferFrom(token2, owner2, owner1, amount2);
+        _safeTransferFrom(blessingToken, owner1, owner2, amount1);
+        _safeTransferFrom(web3bridgeToken, owner2, owner1, amount2);
     }
 
     function _safeTransferFrom(
