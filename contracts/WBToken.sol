bd5986250f43b10a10e85def128f231528cbc2ff
dicetheDev <samuelblessing125@gmail.com>
2022-08-17 18:49:15 +0100

    lastest push

--- contracts/WBToken.sol
+++ contracts/WBToken.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.9;
 import './IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract BlessingToken is ERC20 {
-    constructor() ERC20("BlessingToken", "BTN") {
+contract Web3bridgeToken is ERC20 {
+    constructor() ERC20("Web3bridgeToken", "WBT") {
         // Minting 100 tokens to the owner address
         _mint(msg.sender, 100 * 10**(decimals()));
     }
