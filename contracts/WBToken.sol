6303e89b351e7925d11a7782a9fa470d28398938
dicetheDev <samuelblessing125@gmail.com>
2022-08-17 18:55:45 +0100

    initial commit

--- contracts/WBToken.sol
+++ contracts/WBToken.sol
@@ -1,12 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import './IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Web3bridgeToken is ERC20 {
     constructor() ERC20("Web3bridgeToken", "WBT") {
-        // Minting 100 tokens to the owner address
         _mint(msg.sender, 100 * 10**(decimals()));
     }
 }
